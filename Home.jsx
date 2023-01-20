55555c6a6fe4f377ce17bd91c66733052d58ff88
Thad Szyplik <119820960+ThadSzyplik@users.noreply.github.com>
2023-01-20 09:15:05 -0800

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,14 +1,54 @@
 import React from "react";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
+import userService from "../userService";
+import toastr from "toastr";
+import { useState } from "react";
 
 
-function Home(props) {
+function Home() {
+
+  useEffect(()=> {
+    userService.getCurrentUser().then(currentUserSuccess).catch(currentUserError)
+  }, []);
+
+  function currentUserSuccess(response){
+    let userInfo = response.data.item.name
+    console.log("this is current user success handler")
+    // onUserLogg
+    toastr.success("Welcome back, Friend!")
+  
+    setCurrentUser((prevState) => {
+      console.log("updater onChange");
+  
+      const user = {
+      ...prevState, 
+  };
+      user.name= userInfo
+       return user;
+  
+        });
+  
+  }
+  
+  function currentUserError(error){
+    console.warn(error)
+    toastr.error("No current User!")
+  }
+        const [currentUser, setCurrentUser ] = useState({
+          firstName:"",
+        });
+
   return (
+    
     <React.Fragment>
       <h1>
-        Hello, {props.user ? props.user?.firstName : ""} {props.user ? props.user?.lastName : ""}   
+        Hello, {currentUser.name}
+
+        
+        {/* {props.userData ? props.userData?.firstName : ""} {props.userData ? props.userData?.lastName : ""}    */}
       </h1>
-      <hr />
+      {/* <hr /> */}
       <Footer></Footer>
     </React.Fragment>
   );
