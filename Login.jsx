e0c5b88552de61cfb4c3a3d8f197802121d91091
Thad Szyplik <119820960+ThadSzyplik@users.noreply.github.com>
2023-01-20 09:44:34 -0800

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,28 +1,98 @@
-import React from "react";
-// import Form from 'react-bootstrap/Form';
-
+import React, { useState } from "react";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
+import userService from "../../userService.js";
 
 function LoginForm() {
-    return (
-        <React.Fragment>
-        <form>
-        <div className="form-group"
-        >
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div className="form-group"
-        >
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-      </React.Fragment>
-    );
-   
-  }
-  
-  export default LoginForm;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    tenantId: "U04CSBS07FX",
+  });
 
+  const onFormFieldChange = (e) => {
+    console.log("onChange", { syntheticEvent: e });
+
+    const target = e.target;
+
+    const value = target.value;
+
+    const name = target.name;
+
+    setUser((prevState) => {
+      console.log("updater onChange");
+
+      const updatedFormData = {
+        ...prevState,
+      };
+
+      updatedFormData[name] = value;
+
+      return updatedFormData;
+    });
+    console.log("end onChange");
+  };
+
+  const onLoginClicked = (e) => {
+    e.preventDefault();
+    userService.loginUser(user).then(onLoginSuccess).catch(onLoginError);
+    // console.log(addUser)
+  };
+
+  const onLoginSuccess = (response) => {
+    console.log(response);
+    toastr.success("Welcome back, Friend!");
+    userService.getCurrentUser().then().catch();
+    // getCurrent user
+  };
+
+  const onLoginError = (error) => {
+    console.log(error);
+    toastr.error("Failed to log in user, try again!");
+  };
+  return (
+    <React.Fragment>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Enter email"
+            name="email"
+            onChange={onFormFieldChange}
+            /* value={user.email} */
+          ></input>
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+
+        <div
+          className="form-group"
+          // value={emailAddress}
+        >
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="password"
+            /* value={user.password} */
+            onChange={onFormFieldChange}
+            placeholder="Password"
+          ></input>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={onLoginClicked}
+        >
+          Submit
+        </button>
+      </form>
+    </React.Fragment>
+  );
+}
+export default LoginForm;
