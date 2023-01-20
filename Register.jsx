9da7b4a1d6f88b9c52ef2911ff52e0722b143a66
Thad Szyplik <119820960+ThadSzyplik@users.noreply.github.com>
2023-01-20 09:41:28 -0800

    Update Register.jsx

--- Register.jsx
+++ Register.jsx
@@ -1,57 +1,165 @@
 /*
 this will have 
-
 email address:
 First Name:
 Last Name:
 Password : MUST MATCH CONFIRM
 Confirm Password: MUST MATCH PASSWORD
 Profile URL: will be the avatar URL in the form.... find how they communicate
-
 ALSO need a tenandId created but need to ensure they 1. don't show on form, and 2. communicate with register user.
+*/
 
-*/ 
-
-import React from "react";
-
+import React, { useState } from "react";
+// import { addUser, userService } from "../../userService";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
+import userService from "../../userService.js";
 
 function RegisterForm() {
-    return (
-        <React.Fragment>
-        <form>
-        <div className="form-group"
-        >
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="floatingEmailInput" aria-describedby="emailHelp" placeholder="Enter email"></input>
-         
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    avatarUrl: "",
+    password: "",
+    passwordConfirm: "",
+    tenantId: "U04CSBS07FX",
+  });
+
+  const onFormFieldChange = (e) => {
+    console.log("onChange", { syntheticEvent: e });
+
+    const target = e.target;
+
+    const value = target.value;
+
+    const name = target.name;
+
+    setUser((prevState) => {
+      console.log("updater onChange");
+
+      const updatedFormData = {
+        ...prevState,
+      };
+
+      updatedFormData[name] = value;
+
+      return updatedFormData;
+    });
+    console.log("end onChange");
+  };
+
+  const onRegisterClicked = (e) => {
+    e.preventDefault();
+    userService.addUser(user).then(onRegisterSuccess).catch(onRegisterError);
+    // console.log(addUser)
+  };
+
+  const onRegisterSuccess = (response) => {
+    console.log(response);
+    toastr.success("User Created!");
+  };
+
+  const onRegisterError = (error) => {
+    console.log(error);
+    toastr.error("cannot create user");
+  };
+  return (
+    <React.Fragment>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="floatingEmailInput"
+            placeholder="Enter email"
+            name="email"
+            // value={user.email}
+            onChange={onFormFieldChange}
+          ></input>
         </div>
-        <div className="form-group"
-        >
-          <label for="exampleInputFirstName">First Name</label>
-          <input type="firstName" class="form-control" id="floatingFirstNameInput" aria-describedby="emailHelp" placeholder="Enter First Name"></input>
+        <div className="form-group">
+          <label htmlFor="exampleInputFirstName">First Name</label>
+          <input
+            type="firstName"
+            className="form-control"
+            id="floatingFirstNameInput"
+            placeholder="Enter First Name"
+            name="firstName"
+            // value={user.firstName}
+            onChange={onFormFieldChange}
+          ></input>
         </div>
 
-        <div className="form-group"
-        >
-          <label for="exampleInputLastName">Last Name</label>
-          <input type="lastName" class="form-control" id="floatingLastNameInput" aria-describedby="emailHelp" placeholder="Enter First Name"></input>
+        <div className="form-group">
+          <label htmlFor="exampleInputLastName">Last Name</label>
+          <input
+            type="lastName"
+            className="form-control"
+            id="floatingLastNameInput"
+            placeholder="Enter Last Name"
+            name="lastName"
+            // value={user.lastName}
+            onChange={onFormFieldChange}
+          ></input>
         </div>
 
-        <div className="form-group"
-        >
-          <label for="passwordInput">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+        <div className="form-group">
+          <label htmlFor="exampleURL">Profile URL </label>
+          <input
+            type="URL"
+            className="form-control"
+            id="floatingURLInput"
+            placeholder="Enter Desired URL"
+            name="avatarUrl"
+            // value={user.avatarUrl}
+            onChange={onFormFieldChange}
+          ></input>
         </div>
-        <div className="form-group"
-        >
-          <label for="confirmPassword">Confirm Password</label>
-          <input type="password" class="form-control" id="floatingConfirmPassword1" placeholder="Password"></input>
+
+        <div className="form-group">
+          <label htmlFor="passwordInput">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            name="password"
+            // value={user.password}
+            onChange={onFormFieldChange}
+          ></input>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="floatingConfirmPassword1"
+            placeholder="Confirm Password"
+            name="passwordConfirm"
+            // value={user.passwordConfirm}
+            onChange={onFormFieldChange}
+          ></input>
+        </div>
+        <button
+          type="submit"
+          class="btn btn-primary"
+          onClick={onRegisterClicked}
+        >
+          Register
+        </button>
       </form>
-      </React.Fragment>
-    );
-   
-  }
-  
-  export default RegisterForm;
+      {/* <div>
+        <h4>
+            Output
+        </h4>
+        <pre>
+            <code>{JSON.stringify(setState, undefined, 2)}</code>
+        </pre>
+      </div> */}
+    </React.Fragment>
+  );
+}
+
+export default RegisterForm;
